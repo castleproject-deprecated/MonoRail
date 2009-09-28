8099b702ebb3db162ce2dcd2a451702d5ac43707
Jonathon Rossi <jono@jonorossi.com>
2009-09-28 13:49:41 +0000

    Setting NVelocity's version to 1.1.0.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/NVelocity/trunk@6015 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- NVelocity/AssemblyInfo.cs
+++ NVelocity/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Castle Project")]
 [assembly: AssemblyProduct("NVelocity")]
 [assembly: AssemblyCopyright("Castle Project, original author or authors")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyDelaySign(false)]
