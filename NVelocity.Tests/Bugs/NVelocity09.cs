378167165091df8195e6667d93183ee8134a75fd
hconceicao <henry.conceicao@gmail.com>
2010-08-23 16:32:18 -0300

    issues regression tests

---
+++ NVelocity.Tests/Bugs/NVelocity09.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NVelocity.Tests.Bugs
+{
+	using System.IO;
+	using App;
+	using Commons.Collections;
+	using NUnit.Framework;
+	using Runtime;
+	using Test;
+
+	[TestFixture, Ignore("Won't fix.")]
+	public class NVelocity09 : BaseTestCase
+	{
+		[Test]
+		public void Test()
+		{
+			var velocityEngine = new VelocityEngine();
+
+			ExtendedProperties extendedProperties = new ExtendedProperties();
+			extendedProperties.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, TemplateTest.FILE_RESOURCE_LOADER_PATH);
+
+			velocityEngine.Init(extendedProperties);
+
+			VelocityContext context = new VelocityContext();
+
+			Template template = velocityEngine.GetTemplate(
+				GetFileName(null, "nv09", TemplateTest.TMPL_FILE_EXT));
+
+			StringWriter writer = new StringWriter();
+
+			template.Merge(context, writer);
+		}
+	}
+}
---
+++ NVelocity.Tests/Bugs/NVelocity14.cs
@@ -0,0 +1,37 @@
+﻿namespace NVelocity.Tests.Bugs
+{
+	using System;
+	using System.IO;
+	using App;
+	using Commons.Collections;
+	using Exception;
+	using NUnit.Framework;
+	using Runtime;
+	using Test;
+
+	[TestFixture]
+	public class NVelocity14 : BaseTestCase
+	{
+		[Test, ExpectedException(typeof(ParseErrorException))]
+		public void Test()
+		{
+			var velocityEngine = new VelocityEngine();
+
+			ExtendedProperties extendedProperties = new ExtendedProperties();
+			extendedProperties.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, TemplateTest.FILE_RESOURCE_LOADER_PATH);
+
+			velocityEngine.Init(extendedProperties);
+
+			VelocityContext context = new VelocityContext();
+
+			Template template = velocityEngine.GetTemplate(
+				GetFileName(null, "nv14", TemplateTest.TMPL_FILE_EXT));
+
+			StringWriter writer = new StringWriter();
+
+			template.Merge(context, writer);
+
+			Console.WriteLine(writer);
+		}
+	}
+}
