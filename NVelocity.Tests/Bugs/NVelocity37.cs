9284f59c21dc97b3e6f8414efeb1ad59fac17e9a
hconceicao <henry.conceicao@gmail.com>
2010-08-23 16:46:16 -0300

    issues regression tests

---
+++ NVelocity.Tests/Bugs/NVelocity37.cs
@@ -0,0 +1,42 @@
+﻿namespace NVelocity.Tests.Bugs
+{
+	using System;
+	using System.IO;
+	using App;
+	using Commons.Collections;
+	using NUnit.Framework;
+	using Runtime;
+	using Test;
+
+	[TestFixture]
+	public class NVelocity37 : BaseTestCase
+	{
+		[Test]
+		public void Test()
+		{
+			var velocityEngine = new VelocityEngine();
+
+			ExtendedProperties extendedProperties = new ExtendedProperties();
+			extendedProperties.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, TemplateTest.FILE_RESOURCE_LOADER_PATH);
+
+			velocityEngine.Init(extendedProperties);
+
+			VelocityContext context = new VelocityContext();
+
+			context.Put("yada", "line");
+
+			Template template = velocityEngine.GetTemplate(
+				GetFileName(null, "nv37", TemplateTest.TMPL_FILE_EXT));
+
+			StringWriter writer = new StringWriter();
+
+#pragma warning disable 612,618
+			velocityEngine.MergeTemplate("nv37.vm", context, writer);
+#pragma warning restore 612,618
+
+			//template.Merge(context, writer);
+
+			Console.WriteLine(writer);
+		}
+	}
+}
