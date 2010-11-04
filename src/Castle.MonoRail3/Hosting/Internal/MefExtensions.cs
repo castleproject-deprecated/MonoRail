44957aa2ed35f6d021026e3cb51db9e535dd2e08
hconceicao <henry.conceicao@gmail.com>
2010-11-04 21:36:08 -0200

    pending changes on mefextensions

--- src/Castle.MonoRail3/Hosting/Internal/MefExtensions.cs
+++ src/Castle.MonoRail3/Hosting/Internal/MefExtensions.cs
@@ -1,1 +1,42 @@
-﻿
+﻿namespace Castle.MonoRail3.Hosting.Internal
+{
+	using System.ComponentModel.Composition;
+	using System.ComponentModel.Composition.Hosting;
+	using System.ComponentModel.Composition.Primitives;
+	using System.Web;
+
+	public static class MefExtensions
+	{
+		// Extension method to simplify filtering expression
+		public static bool IsShared(this ComposablePartDefinition part)
+		{
+			object value;
+
+			if (part.Metadata.TryGetValue(CompositionConstants.PartCreationPolicyMetadataName, out value))
+			{
+				return ((CreationPolicy) value) == CreationPolicy.Shared;
+			}
+
+			return false;
+		}
+
+		public static CompositionBatch Compose(this CompositionContainer container, IComposableHandler handler)
+		{
+			var batch = new CompositionBatch();
+
+			batch.AddPart(handler);
+
+			container.Compose(batch);
+
+			return batch;
+		}
+
+		public static CompositionContainer HookOn(this CompositionContainer container, HttpContext httpContext)
+		{
+			httpContext.Items[ContainerManager.RequestContainerKey] = container;
+			httpContext.ApplicationInstance.EndRequest += ContainerManager.OnEndRequestDisposeContainer;
+
+			return container;
+		}
+	}
+}
