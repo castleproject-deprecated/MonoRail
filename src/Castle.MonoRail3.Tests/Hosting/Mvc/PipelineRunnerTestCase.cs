68a0372d3f46724822daaf83afbf831ab4e3ad88
hconceicao <henry.conceicao@gmail.com>
2010-11-17 17:26:09 -0200

    added license header

--- src/Castle.MonoRail3.Tests/Hosting/Mvc/PipelineRunnerTestCase.cs
+++ src/Castle.MonoRail3.Tests/Hosting/Mvc/PipelineRunnerTestCase.cs
@@ -1,11 +1,24 @@
-﻿namespace Castle.MonoRail3.Tests.Hosting.Mvc
+﻿//  Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+namespace Castle.MonoRail3.Tests.Hosting.Mvc
 {
 	using System.Web;
-	using System.Web.Routing;	
+	using System.Web.Routing;
+	using MonoRail3.Hosting.Mvc;
 	using Moq;
 	using NUnit.Framework;
-	using Castle.MonoRail3.Hosting.Mvc;
-	using Castle.MonoRail3.Primitives.Mvc;
 	using Primitives;
 
 	[TestFixture]
