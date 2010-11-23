0513c3ba750a46d6086b1f560610dc8de4c75729
hammett <hammett@gmail.com>
2010-11-22 23:17:24 -0800

    Licenses changes to region

--- src/Castle.MonoRail/IMonoRailServices.cs
+++ src/Castle.MonoRail/IMonoRailServices.cs
@@ -1,4 +1,5 @@
-﻿//  Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+﻿#region License
+//  Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 //  
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -11,7 +12,8 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-// 
+#endregion
+
 namespace Castle.MonoRail
 {
     using System;
