d8447457c51e05a9e7120528d52cb034d7f2e15b
hammett <hammett@gmail.com>
2011-04-25 19:47:47 -0700

    two simple scripts to give an overview of the mef components within the framework

---
+++ ComponentsPerScope.fsx
@@ -0,0 +1,41 @@
+﻿
+let targetFolder = @".\src\Castle.MonoRail\bin\Debug"
+
+#I @"src\Castle.MonoRail\bin\Debug"
+#r "System.ComponentModel.Composition.Codeplex.dll"
+#r "Castle.MonoRail.dll"
+
+open System.Reflection
+open System.IO
+open System.ComponentModel.Composition
+
+let asm = Assembly.LoadFrom(Path.Combine(targetFolder, "Castle.MonoRail.dll"))
+
+let loaded_types = 
+    try
+        asm.GetTypes()
+    with
+    | :? ReflectionTypeLoadException as ex -> ex.Types
+
+let exports = 
+    loaded_types 
+    |> Seq.filter (fun t -> t <> null && t.GetCustomAttributes(typeof<ExportAttribute>, true).Length <> 0 )
+
+let appcomponents = 
+    exports
+    |> Seq.filter (fun t -> not (t.IsDefined(typeof<PartMetadataAttribute>, true)) || 
+                            (t.GetCustomAttributes(typeof<PartMetadataAttribute>, true).[0] :?> PartMetadataAttribute).Value :?> Castle.MonoRail.Extensibility.ComponentScope = Castle.MonoRail.Extensibility.ComponentScope.Application )
+    
+let reqcomponents = 
+    exports
+    |> Seq.filter (fun t -> (t.IsDefined(typeof<PartMetadataAttribute>, true)) &&  
+                            (t.GetCustomAttributes(typeof<PartMetadataAttribute>, true).[0] :?> PartMetadataAttribute).Value :?> Castle.MonoRail.Extensibility.ComponentScope = Castle.MonoRail.Extensibility.ComponentScope.Request )
+
+printf "App level components: \r\n"
+for t in appcomponents do
+    printf "   %s \r\n" t.FullName
+            
+printf "Req level components: \r\n"
+for t in reqcomponents do
+    printf "   %s \r\n" t.FullName
+
---
+++ ListAllExports.fsx
@@ -0,0 +1,41 @@
+﻿
+let targetFolder = @".\src\Castle.MonoRail\bin\Debug"
+
+#I @"src\Castle.MonoRail\bin\Debug"
+#r "System.ComponentModel.Composition.Codeplex.dll"
+
+open System.Reflection
+open System.IO
+open System.ComponentModel.Composition
+
+let asm = Assembly.LoadFrom(Path.Combine(targetFolder, "Castle.MonoRail.dll"))
+
+let loaded_types = 
+    try
+        asm.GetTypes()
+    with
+    | :? ReflectionTypeLoadException as ex -> ex.Types
+
+let exports = 
+    loaded_types 
+    |> Seq.filter (fun t -> t <> null && t.GetCustomAttributes(typeof<ExportAttribute>, true).Length <> 0 )
+    |> Seq.map (fun t -> (t, (t.GetCustomAttributes(typeof<ExportAttribute>, true).[0] :?> ExportAttribute).ContractType) )
+    |> Seq.groupBy (fun (t,e) -> t.Namespace)
+
+for (ns,types) in exports do
+    printf "%s: \r\n" ns
+
+    for t1,t2 in types do 
+        if t2 = null then
+            printf "\tContract: %s  implemented by  %s \r\n" t1.Name t1.Name
+        else
+            printf "\tContract: %s  implemented by  %s \r\n" t2.Name t1.Name
+
+        let meta = 
+            t1.GetCustomAttributes(typeof<ExportMetadataAttribute>, true) 
+            |> Seq.map (fun m -> m :?> ExportMetadataAttribute)
+
+        for m in meta do
+            printf "\t   Metadata: %s  =  %s \r\n" m.Name (m.Value.ToString())
+            
+
