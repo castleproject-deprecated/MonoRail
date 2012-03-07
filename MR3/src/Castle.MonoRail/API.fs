32a0aa7e1d3b73ac04679e84061009f6947da320
hammett <hammett@gmail.com>
2012-03-07 12:24:43 -0800

    Removing type alias

--- MR3/src/Castle.MonoRail/API.fs
+++ MR3/src/Castle.MonoRail/API.fs
@@ -32,7 +32,8 @@ namespace Castle.MonoRail
     type Attributes() =
         inherit Dictionary<string,string>()
 
-    type Options = Dictionary<obj,obj>
+    type Options() = 
+        inherit Dictionary<obj,obj>()
 
 
     [<Interface; AllowNullLiteral>]
