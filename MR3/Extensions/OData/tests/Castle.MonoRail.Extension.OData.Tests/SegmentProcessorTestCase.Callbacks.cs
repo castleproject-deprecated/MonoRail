3c1eac810259959a822aee252a6072c7c95f9b5a
hamilton verissimo <hammett@gmail.com>
2012-04-12 18:45:14 -0700

    placeholder for callback tests

---
+++ MR3/Extensions/OData/tests/Castle.MonoRail.Extension.OData.Tests/SegmentProcessorTestCase.Callbacks.cs
@@ -0,0 +1,22 @@
+﻿namespace Castle.MonoRail.Extension.OData.Tests
+{
+    using System;
+    using System.Data.Services.Common;
+    using System.IO;
+    using System.ServiceModel.Syndication;
+    using System.Text;
+    using System.Xml;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+	// todo: tests all callbacks: access[single/many], create/update/delete
+    public partial class SegmentProcessorTestCase
+    {
+        // naming convention for testing methods
+        // [EntitySet|EntityType|PropSingle|PropCollection|Complex|Primitive]_[Operation]_[InputFormat]_[OutputFormat]__[Success|Failure]
+
+        
+
+
+    }
+}
