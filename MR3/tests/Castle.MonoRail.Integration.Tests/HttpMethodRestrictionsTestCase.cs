8255c3ed798bd1dae5d3991b184216cb479fcfa6
hammett <hammett@gmail.com>
2012-04-21 01:04:48 -0700

    more tests

---
+++ MR3/tests/Castle.MonoRail.Integration.Tests/HttpMethodRestrictionsTestCase.cs
@@ -0,0 +1,75 @@
+﻿namespace Castle.MonoRail.Integration.Tests
+{
+	using System;
+	using System.IO;
+	using System.Net;
+	using FluentAssertions;
+	using NUnit.Framework;
+
+	[TestFixture, Category("Integration")]
+	public class HttpMethodRestrictionsTestCase : BaseServerTest
+	{
+		[Test]
+		public void InvokesGetActionPrefixed()
+		{
+			var req = WebRequest.CreateDefault(new Uri(BuildUrl("/HttpMethodRestrictions/index")));
+			var reply = (HttpWebResponse) req.GetResponse();
+
+			reply.StatusCode.Should().Be(HttpStatusCode.OK);
+			new StreamReader(reply.GetResponseStream()).ReadToEnd().Should().Contain("succeeded - action prefixed with Get_");			
+		}
+
+		[Test]
+		public void InvokesGetActionNotPrefixedAndNotAnnotated()
+		{
+			var req = WebRequest.CreateDefault(new Uri(BuildUrl("/HttpMethodRestrictions/Index3")));
+			var reply = (HttpWebResponse)req.GetResponse();
+
+			reply.StatusCode.Should().Be(HttpStatusCode.OK);
+			new StreamReader(reply.GetResponseStream()).ReadToEnd().Should().Contain("succeeded - action without att or prefix");
+		}
+
+		[Test]
+		public void InvokesGetActionAnnotated()
+		{
+			var req = WebRequest.CreateDefault(new Uri(BuildUrl("/HttpMethodRestrictions/Index2")));
+			var reply = (HttpWebResponse)req.GetResponse();
+
+			reply.StatusCode.Should().Be(HttpStatusCode.OK);
+			new StreamReader(reply.GetResponseStream()).ReadToEnd().Should().Contain("succeeded - action with httpmethodattribute (get)");
+		}
+
+		[Test]
+		public void InvokesPostActionPrefixed()
+		{
+			var req = (HttpWebRequest) WebRequest.CreateDefault(new Uri(BuildUrl("/HttpMethodRestrictions/index")));
+			req.Method = "POST";
+			var reply = (HttpWebResponse)req.GetResponse();
+
+			reply.StatusCode.Should().Be(HttpStatusCode.OK);
+			new StreamReader(reply.GetResponseStream()).ReadToEnd().Should().Contain("succeeded post - action prefixed");
+		}
+
+		[Test]
+		public void InvokesPostActionAnnotated()
+		{
+			var req = (HttpWebRequest)WebRequest.CreateDefault(new Uri(BuildUrl("/HttpMethodRestrictions/index4")));
+			req.Method = "POST";
+			var reply = (HttpWebResponse)req.GetResponse();
+
+			reply.StatusCode.Should().Be(HttpStatusCode.OK);
+			new StreamReader(reply.GetResponseStream()).ReadToEnd().Should().Contain("succeeded post/put - action with httpmethodattribute");
+		}
+
+		[Test]
+		public void InvokesPutActionAnnotated()
+		{
+			var req = (HttpWebRequest)WebRequest.CreateDefault(new Uri(BuildUrl("/HttpMethodRestrictions/index4")));
+			req.Method = "PUT";
+			var reply = (HttpWebResponse)req.GetResponse();
+
+			reply.StatusCode.Should().Be(HttpStatusCode.OK);
+			new StreamReader(reply.GetResponseStream()).ReadToEnd().Should().Contain("succeeded post/put - action with httpmethodattribute");
+		}
+	}
+}
