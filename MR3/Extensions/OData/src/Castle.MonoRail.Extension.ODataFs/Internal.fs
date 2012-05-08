b8a42d5403e0f5fb461df3d2f6705868d221066a
Cristian Danes <CrisD@iqmetrix.com>
2012-05-08 14:59:53 -0700

    attempt to fix complex collection type serialization issue

---
+++ MR3/Extensions/OData/src/Castle.MonoRail.Extension.ODataFs/Internal.fs
@@ -0,0 +1,12 @@
+﻿module InternalUtils 
+
+    open System
+    open System.Reflection
+    open System.Collections.Generic
+
+
+    let getEnumerableElementType (possibleEnumerableType:Type) = 
+        let found = possibleEnumerableType.FindInterfaces(TypeFilter(fun t o -> (o :?> Type).IsAssignableFrom(t)), typedefof<IEnumerable<_>>) 
+        if found.Length = 0
+        then None
+        else Some(found.[0].GetGenericArguments().[0])
