6d120b668a1605896fe562b957a75db9a8e12fcf
hammett <hammett@gmail.com>
2012-08-30 21:38:46 -0700

    started integration tests for metadata serialization

---
+++ MR3/Extensions/OData/tests/Castle.MonoRail.Extension.OData.Integration.Tests/MetadataTestCase.cs
@@ -0,0 +1,47 @@
+﻿namespace Castle.MonoRail.Extension.OData.Integration.Tests
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using FluentAssertions;
+    using MonoRail.Integration.Tests;
+    using NUnit.Framework;
+
+    [TestFixture, Category("Integration")]
+    public class MetadataTestCase : BaseServerTest
+	{
+        public MetadataTestCase()
+		{
+			this.WebSiteFolder = "ODataTestWebSite";
+		}
+
+        [Test]
+        public void metadata_for_root_model()
+        {
+            var req = (HttpWebRequest)WebRequest.CreateDefault(new Uri(BuildUrl("/models/RootModel/$metadata")));
+            req.Accept = "*/*";
+            req.Method = "GET";
+            var reply = (HttpWebResponse)req.GetResponse();
+            reply.StatusCode.Should().Be(HttpStatusCode.OK);
+            reply.ContentType.Should().Be("application/xml; charset=utf-8");
+            var replyContent = new StreamReader(reply.GetResponseStream()).ReadToEnd();
+            replyContent.Should().BeEquivalentTo(@"{
+	""d"": [
+		{
+			""__metadata"": {
+				""uri"": ""http://localhost:1302/models/Repo/Repositories(1)"",
+				""type"": ""ns.Repository""
+			},
+			""Id"": 1,
+			""Name"": ""repo1"",
+			""Branches"": {
+				""__deferred"": {
+					""uri"": ""http://localhost:1302/models/Repo/Repositories(1)/Branches""
+				}
+			}
+		}
+	]
+}");
+        }
+    }
+}
